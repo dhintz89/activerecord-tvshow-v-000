d2c4d73d68c4e8a89e7ae47a6bd84e8ad8b95d39
Dan Hintz <dhintz@live.com>
2018-12-26 22:01:50 +0000

    Automatically backed up by Learn

---
+++ app/models/show.rb
@@ -0,0 +1,12 @@
+class Show < ActiveRecord::Base
+  
+  def self.highest_rating
+    Show.maximum(:rating)
+  end
+  
+  def self.most_popular_show
+    self.find_by(rating: highest_rating)
+  end
+  
+  
+end
